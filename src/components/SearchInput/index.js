09151593dd5c16e5a901dc18f710a42f4cd71128
jxhnkndl <jkroyston@gmail.com>
2021-04-08 18:19:46 -0400

    Create search input component directory

---
+++ src/components/SearchInput/index.js
@@ -0,0 +1,12 @@
+// Import modules
+import React from 'react';
+
+// Search input component function
+function SearchInput() {
+  return (
+    <p>SEARCH INPUT</p>
+  );
+}
+
+// Export search input component
+export default SearchInput;
