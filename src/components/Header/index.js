dbaf652905264c6b6214f9f19e59c6057dd01daf
jxhnkndl <jkroyston@gmail.com>
2021-04-08 17:58:20 -0400

    Create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+// Import modules
+import React from 'react';
+
+// Header component function
+function Header() {
+  return (
+    <header>
+      <h1>Employee Directory</h1>
+    </header>
+  );
+}
+
+// Export Header
+export default Header;
