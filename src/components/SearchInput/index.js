606677d231108beee3bc5107196dab169a2043c2
jxhnkndl <jkroyston@gmail.com>
2021-04-09 14:55:03 -0400

    Create basic input component to use as a search field for filtering results

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -4,7 +4,10 @@ import React from 'react';
 // Search input component function
 function SearchInput() {
   return (
-    <p>SEARCH INPUT</p>
+    <div class="form-group py-4">
+      <label htmlFor="search-input">Filter employees by name or phone number:</label>
+      <input type="text" class="form-control" id="search-input" />
+    </div>
   );
 }
 
