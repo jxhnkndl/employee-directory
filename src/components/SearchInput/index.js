2fb16e47391f1e8e213c0b4cbd3e4f9c19c12082
jxhnkndl <jkroyston@gmail.com>
2021-04-09 15:18:23 -0400

    Replace instances of the class attribute with className

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -4,9 +4,9 @@ import React from 'react';
 // Search input component function
 function SearchInput() {
   return (
-    <div class="form-group py-4">
+    <div className="form-group py-4">
       <label htmlFor="search-input">Filter employees by name or phone number:</label>
-      <input type="text" class="form-control" id="search-input" />
+      <input type="text" className="form-control" id="search-input" />
     </div>
   );
 }
