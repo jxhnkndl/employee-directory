4b2a4a3fa64c79856b40a0b97a4fec2e6064fd14
jxhnkndl <jkroyston@gmail.com>
2021-04-09 18:17:21 -0400

    Apply fixed column widths to results table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,17 +1,36 @@
 // Import modules
 import React from 'react';
 
+// Table column widths
+const columns = {
+  image: {
+    width: '10%'
+  },
+  name: {
+    width: '30%'
+  },
+  phone: {
+    width: '20%'
+  },
+  email: {
+    width: '30%'
+  },
+  dob: {
+    width: '10%'
+  }
+}
+
 // Table component function
 function Table(props) {
   return (
     <table className="table table-hover table-striped p-0">
       <thead>
         <tr>
-          <th scope="col">Image</th>
-          <th scope="col">Name</th>
-          <th scope="col">Phone</th>
-          <th scope="col">Email</th>
-          <th scope="col">DOB</th>
+          <th style={columns.image} scope="col">Image</th>
+          <th style={columns.name} scope="col">Name</th>
+          <th style={columns.phone} scope="col">Phone</th>
+          <th style={columns.email} scope="col">Email</th>
+          <th style={columns.dob} scope="col">DOB</th>
         </tr>
       </thead>
       <tbody>
