906d803a6901bcd004a965adfa4c3da27bd9b089
jxhnkndl <jkroyston@gmail.com>
2021-04-09 15:39:27 -0400

    Import API methods, init state object, and add functions for loading employees when Search component mounts

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,10 +3,27 @@ import React, { Component } from 'react';
 import Container from '../components/Container';
 import SearchInput from '../components/SearchInput';
 import Table from '../components/Table';
+import API from '../utils/API';
 
 // Create search results container
 class Search extends Component {
-  
+  state = {
+    search: "",
+    results: []
+  }
+
+  componentDidMount = () => {
+    this.getEmployees();
+  }
+
+  getEmployees = () => {
+    API.getEmployees()
+      .then(res => {
+        this.setState({ results: res.data.results });
+        console.log(this.state);;
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
