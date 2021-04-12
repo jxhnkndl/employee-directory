9975f7ececb2e312a2514fcfef5858adc9295943
jxhnkndl <jkroyston@gmail.com>
2021-04-12 16:06:24 -0400

    Create component stylesheet and apply styling to header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 // Import modules
 import React from 'react';
+import './style.css';
 
 // Header component function
 function Header() {
   return (
-    <header className="bg-primary p-3">
-      <h1 className="h4 mb-0 text-center text-light">
-        Employee Directory
+    <header className="p-4">
+      <h1 className="mb-0 text-center text-light">
+        rollcall
+        <i class="fas fa-comment-dots ml-2"></i>
       </h1>
     </header>
   );
